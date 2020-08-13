cc43972945cd762755abbec3947f87acbe12aba9
Viennarz Curtiz <viennarzc@gmail.com>
2020-08-14 07:00:58 +0800

    Create Model - UserProfile

---
+++ VVCGithubUsers/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  VVCGithubUsers
+//
+//  Created by SCI-Viennarz on 8/14/20.
+//  Copyright © 2020 VVC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
