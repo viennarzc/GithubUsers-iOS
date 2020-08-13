24140aa642567428aa5e8fee37f165e3cc87b047
Viennarz Curtiz <viennarzc@gmail.com>
2020-08-13 16:54:07 +0800

    Add Unit tests

---
+++ VVCGithubUsersTests/UserListTests.swift
@@ -0,0 +1,26 @@
+//
+//  UserListTests.swift
+//  VVCGithubUsersTests
+//
+//  Created by SCI-Viennarz on 8/13/20.
+//  Copyright © 2020 VVC. All rights reserved.
+//
+
+import XCTest
+
+@testable import VVCGithubUsers
+
+class UserListTests: XCTestCase {
+  
+  func testRequestGithubUsersHasData() {
+    NetworkManager.shared.fetchUsers { (result) in
+      switch result {
+      case .failure(let error):
+        print(error)
+      case .success(let users):
+        XCTAssert(!users.isEmpty)
+      }
+    }
+  }
+
+}
