6572b73c5146c172092bde52143c54111705de55
Viennarz Curtiz <viennarzc@gmail.com>
2020-08-13 09:00:11 +0800

    Create Protocols - UserTableCell

---
+++ VVCGithubUsers/UsersList/UserTableCell.swift
@@ -0,0 +1,25 @@
+//
+//  UserTableCell.swift
+//  VVCGithubUsers
+//
+//  Created by SCI-Viennarz on 8/13/20.
+//  Copyright © 2020 VVC. All rights reserved.
+//
+
+import Foundation
+
+protocol UserTableCell {
+  
+}
+
+protocol Normal: UserTableCell {
+  
+}
+
+protocol Notable: UserTableCell {
+  var hasNotes: Bool { get set}
+}
+
+protocol Invertable: UserTableCell {
+  var isInverted: Bool { get set }
+}
