0a3180b8fb93d27b78dac37b0eeb2066781f03b7
Viennarz Curtiz <viennarzc@gmail.com>
2020-08-14 08:03:53 +0800

    Profile view model Bind to UI

--- VVCGithubUsers/Profile/ProfileViewController.swift
+++ VVCGithubUsers/Profile/ProfileViewController.swift
@@ -10,7 +10,42 @@ import Foundation
 import UIKit
 
 class ProfileViewController: UIViewController {
+  
+  @IBOutlet weak var userAvatar: UIImageView!
+  @IBOutlet weak var followersLabel: UILabel!
+  @IBOutlet weak var followingLabel: UILabel!
+  @IBOutlet weak var userNameLabel: UILabel!
+  @IBOutlet weak var companyLabel: UILabel!
+  @IBOutlet weak var blogLabel: UILabel!
+  
+  
+  var viewModel: ProfileViewModel?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    viewModel?.fetchUserProfile { (error) in
+      if let error = error {
+        print(error)
+        return
+      }
+      
+      self.updateUI()
+    }
+    
+  }
+  
+  func updateUI() {
+    guard let vm = viewModel else { return }
+  
+    if let userProfile = vm.userProfile {
+      followersLabel.text = "\(userProfile.followers)"
+      followingLabel.text = "\(userProfile.following)"
+      userNameLabel.text = userProfile.name
+    }
+    
+    
   }
 }
+
+
